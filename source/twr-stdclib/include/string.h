097c6133e7654bdab25a7b99ba8b6204948d8f90
Anthony <62391911+ajwood1965@users.noreply.github.com>
2024-05-05 22:25:59 -0500

    update

--- source/twr-stdclib/include/string.h
+++ source/twr-stdclib/include/string.h
@@ -3,6 +3,7 @@
 #define __TINY_STRING_H__
 
 #include <_stdtypes.h>
+#include <locale.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -17,12 +18,17 @@ int strcmp(const char* string1, const char* string2);
 int strncmp(const char* lhs, const char* rhs, size_t count);
 int stricmp(const char* string1, const char* string2);
 int strnicmp(const char* string1, const char* string2, size_t count);
+int strcoll(const char* lhs, const char* rhs);
+int strcoll_l(const char* lhs, const char* rhs,  locale_t __attribute__((__unused__)) loc);
+size_t strxfrm(char *dest, const char *source, size_t count);
+size_t strxfrm_l(char *dest, const char *source, size_t count, locale_t __attribute__((__unused__)) locale);
 char *strstr(const char *haystack, const char *needle);
 char * strerror(int errnum );
 char * _strerror(const char *strErrMsg);
 void *memmove(void *dest, const void *src, size_t n);
 int memcmp( const void* lhs, const void* rhs, size_t count );
 void bzero (void *to, size_t count);
+
 
 // implemented in memcpy.wat
 void *memcpy(void *dest, const void * src, size_t n);
