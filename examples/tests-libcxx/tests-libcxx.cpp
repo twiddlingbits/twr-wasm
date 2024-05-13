a251cfe64796ac8ac3b2d376e11c81fd3f36124d
Anthony <62391911+ajwood1965@users.noreply.github.com>
2024-05-13 09:37:33 -0500

    very minor makefile cleanup

--- examples/tests-libcxx/tests-libcxx.cpp
+++ examples/tests-libcxx/tests-libcxx.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
+#include <vector>
 #include <string>
+#include <random>
+#include <clocale>
+#include <regex>
 
 extern "C" void testslibcxx() {
 
 	std::cout << "Hello World!" << std::endl;
 
+	//////////////////////////////////////////////
+
 	std::string str = "this is my string!";
    const char* cstr = str.c_str();
 	printf("string: %s\n", cstr);
+	if (str!="this is my string!") {
+		printf("ERROR! %d\n",__LINE__);
+		abort();
+	}
 
    str.push_back('x');
    str.push_back('y');
@@ -15,4 +25,61 @@ extern "C" void testslibcxx() {
 
    cstr = str.c_str();
 	printf("stringmod: %s\n", cstr);
+	if (str!="this is my string!xyz") {
+		printf("ERROR! %d\n",__LINE__);
+		abort();
+	}
+
+	//////////////////////////////////////////////
+
+	std::vector<int> v1, v2;
+
+    v1.push_back(10);
+    v1.push_back(20);
+    v1.push_back(30);
+    v1.push_back(40);
+    v1.push_back(50);
+
+    std::cout << "v1 = ";
+    for (auto& v : v1) {
+        std::cout << v << " ";
+    }
+    std::cout << std::endl;
+
+    v2.assign(v1.begin(), v1.end());
+    std::cout << "v2 = ";
+    for (auto& v : v2){
+        std::cout << v << " ";
+    }
+    std::cout << std::endl;
+
+	if (v1!=v2) {
+		printf("ERROR! %d\n",__LINE__);
+		abort();
+	}
+
+	//////////////////////////////////////////////
+
+   std::setlocale(LC_ALL, "C");
+	std::lconv* lc = std::localeconv();
+   std::cout << "currency symbol: " << lc->currency_symbol
+              << '(' << lc->int_curr_symbol << ")\n";
+
+	//////////////////////////////////////////////
+
+	std::string target2("Drizzle");
+   std::regex rx2(R"(D\w+e)"); // no double backslashes with raw string literal
+
+   auto found = std::regex_match(target2.cbegin(), target2.cend(), rx2);
+   if (found)
+        std::cout << "Regex found in Drizzle" << std::endl;
+	else
+        std::cout << "Regex NOT found in Drizzle" << std::endl;
+
+	if (!found) {
+		printf("ERROR! %d\n",__LINE__);
+		abort();
+	}
+
 }
+
