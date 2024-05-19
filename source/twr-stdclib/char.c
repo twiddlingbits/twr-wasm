aa0cbea0e3c7319a4aa718e5093161af49508ceb
Anthony <62391911+ajwood1965@users.noreply.github.com>
2024-05-19 16:12:14 -0500

    minor formatting update

--- source/twr-stdclib/char.c
+++ source/twr-stdclib/char.c
@@ -153,7 +153,6 @@ int isdigit_l(int c, locale_t loc) {
 	}
 }
 
-//includes letters, digits, punctuation marks, and symbols—basically, any character that visibly marks the terminal or display, excluding space.
 int isgraph_l(int c, locale_t loc) {
 	assert(c==EOF || c>=0 && c<=255);
 
@@ -192,7 +191,6 @@ int isprint_l(int c, locale_t loc) {
 	}
 }
 
-//checks if the given character is any printable character except for space or alphanumeric characters.
 int ispunct_l(int c, locale_t loc) {
 	assert(c==EOF || c>=0 && c<=255);
 	if (is_c_loc(loc) || is_utf8_loc(loc))
@@ -260,6 +258,11 @@ int toupper_l(int c, locale_t loc) {
 		return twrToUpper1252(c);
 }
 
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+
 static int test_all(int pick);
 static int ascii_unit_tests(void);
 static int test_tolower(int pick);
@@ -310,8 +313,6 @@ int char_unit_test(void) {
 	if (!test_tolower(1)) return 0;
 	if (!test_toupper(1)) return 0;
 	
-	
-
 	setlocale(LC_ALL, "C");
 
 	return 1;
@@ -387,7 +388,6 @@ static int ascii_unit_tests(void) {
 	return 1;
 }
 
-
 static int test_do(int (*func)(int c), int a1[], int a2[], int pick) {
 	int *a;
 	a=pick==0?a1:a2;
@@ -399,6 +399,7 @@ static int test_do(int (*func)(int c), int a1[], int a2[], int pick) {
 	return 1;
 }
 
+/*
 static int test_show(char*name, int (*func)(int c), int a1[], int a2[], int pick) {
 	printf("test_show '%s' %d:\n", name, pick);
 	int *a;
@@ -410,6 +411,7 @@ static int test_show(char*name, int (*func)(int c), int a1[], int a2[], int pick
 	}
 	return 1;
 }
+*/
 
 static int test_tolower(int pick) {
 	int a1[]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,221,222,223,224,225,226,227,228,229,230,231,232,233,234,235,236,237,238,239,240,241,242,243,244,245,246,247,248,249,250,251,252,253,254,255};
@@ -432,7 +434,6 @@ static int test_tolower(int pick) {
 			}
 		}
 	}
-
 
 	return 1;
 }
@@ -458,7 +459,6 @@ static int test_toupper(int pick) {
 		}
 	}
 
-
 	return 1;
 }
 
@@ -475,7 +475,7 @@ static int test_isalnum(int pick) {
 
 	a2[136]=1;  // circumflex -- mismatch between windows (test case generation) and javascript
 
-	a2[178]=0;  // sub/super scripts aren't really digits
+	a2[178]=0;  //sub/super scripts aren't really digits
 	a2[179]=0; // sub/super scripts aren't really digits
 	a2[185]=0; // sub/super scripts aren't really digits
 
@@ -522,7 +522,8 @@ static int test_isgraph(int pick) {
 	int a1[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 	int a2[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,0,1,1,1,1,0,1,0,0,1,1,1,1,1,1,1,0,0,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
 
-   a2[128]=1;  // fix errors in windows generated test data
+	// conform to unicode
+   a2[128]=1;  
    a2[136]=1;
    a2[152]=1;
    a2[153]=1;
