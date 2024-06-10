39b7da8801f58965565a1ac0004dbc08484c674d
Anthony <62391911+ajwood1965@users.noreply.github.com>
2024-06-10 07:31:34 -0500

    rename tests-ui to tests-user

--- examples/tests-user/tests-user.cpp
+++ examples/tests-user/tests-user.cpp
@@ -2,13 +2,12 @@
 #include "parsecmd.h"
 #include "terminal.h"
 
-extern "C" void test_ui(void) {
+extern "C" void tests_user(void) {
 
 	twrTerminal myTerm;
-	std::string inputLine;
 
 	while (1) {
-		inputLine=myTerm.getInputLine();
+		std::string& inputLine=myTerm.getInputLine();
 		parseCommand myCmd(inputLine);
 		printf("cmd: %s\n", myCmd.getCommand().c_str());
 	}
