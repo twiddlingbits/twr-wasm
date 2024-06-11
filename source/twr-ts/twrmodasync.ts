6d877eb2a3d4e50d9d21ffdfafbe8e7b16106a85
Anthony <62391911+ajwood1965@users.noreply.github.com>
2024-06-11 08:54:25 -0500

    fix bug in key input wrt non-ascii characters

--- source/twr-ts/twrmodasync.ts
+++ source/twr-ts/twrmodasync.ts
@@ -75,7 +75,7 @@ export class twrWasmModuleAsync extends twrWasmModuleInJSMain {
 	}
 	
 	private keyEventProcess(ev:KeyboardEvent) {
-		if ( !ev.isComposing  && !ev.metaKey && !ev.ctrlKey && !ev.altKey ) {
+		if ( !ev.isComposing  && !ev.metaKey && ev.key!="Control" && ev.key!="Alt" ) {
 			//console.log("keyDownDiv: ",ev.key, ev.code, ev.key.codePointAt(0), ev);
 			if (ev.key.length==1)
 				return ev.key.codePointAt(0);
