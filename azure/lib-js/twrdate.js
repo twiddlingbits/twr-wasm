6cf52ce5389d481aedcc93f7d161bfec613b35ad
Anthony <ajwood1965@gmail.com>
2024-06-17 19:59:35 -0500

    stop tracking build artifacts in dev

--- azure/lib-js/twrdate.js
+++
@@ -1,5 +0,0 @@
-// return ms since epoch as double
-export function twrTimeEpochImpl() {
-    return Date.now();
-}
-//# sourceMappingURL=twrdate.js.map
--- azure/lib-js/twrmod.d.ts
+++
@@ -1,8 +0,0 @@
-import { IModOpts } from "./twrmodbase.js";
-import { twrWasmModuleInJSMain } from "./twrmodjsmain.js";
-export declare class twrWasmModule extends twrWasmModuleInJSMain {
-    malloc: (size: number) => Promise<number>;
-    constructor(opts?: IModOpts);
-    null(inval?: any): void;
-}
-//# sourceMappingURL=twrmod.d.ts.map
