98564ffe85a41670f04ad7bbe5ee90e3715027fe
Anthony <ajwood1965@gmail.com>
2024-08-04 15:31:43 -0500

    remove example build artifacts from dev.  Not sure how they got back in.  They are only checked into main (by merge script)

---
+++ examples/cleanall.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# this script builds all the examples but does not bundle them.
+# also re-builds (not clean) twr-wasm source to help ensure it is current
+# after running this script, examples can be most easily be executed by using the VS Code "Run and Debug" menu
+# or use buildbundle.sh to bundle and then execute with a local server (see buildbundle.sh)
+
+if [ "$(uname -o)" = "Msys" ]; then
+export MSYS_NO_PATHCONV=1
+make="mingw32-make"
+else
+make="make"
+fi
+
+set -e  # exit if any command returns non zero
+
+cd helloworld
+$make clean
+
+cd ../stdio-div
+$make clean
+
+cd ../stdio-canvas
+$make  clean
+
+cd ../multi-io
+$make  clean
+
+cd ../callc
+$make clean
+
+cd ../maze
+$make  clean
+
+cd ../fft
+$make clean
+
+cd ../balls
+$make clean
+
+cd ../tests
+$make clean
+
+cd ../tests-user
+$make clean
+
+cd ../tests-libcxx
+$make clean
+
+cd ../pong
+$make clean
+
+cd ..
+
+
+
+
+
+
