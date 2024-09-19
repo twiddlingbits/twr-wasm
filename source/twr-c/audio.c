5cc81fc4b0e83c6ef717053e4dca611bb20ea811
Johnathon <johnathonmgaines@gmail.com>
2024-09-19 15:07:26 -0500

    Added functions to convert 8bit, 16bit, and 32bit PCM to the float style JS uses

--- source/twr-c/audio.c
+++ source/twr-c/audio.c
@@ -1,5 +1,6 @@
 #include "twr-audio.h"
 #include "twr-jsimports.h"
+#include <stdlib.h>
 
 
 
@@ -37,4 +38,41 @@ long twr_audio_play_range_sync_full(long node_id, long start_sample, long end_sa
    __attribute__((import_name("twrAudioPlayRangeSync"))) long twr_audio_play_range_sync_sample_rate(long node_id, long start_sample, long end_sample, int loop, long sample_rate, double volume, double pan);
 
    return twr_audio_play_range_sync_sample_rate(node_id, start_sample, end_sample, fields->loop, fields->sample_rate, fields->volume, fields->pan);
+}
+
+float* twr_convert_8_bit_pcm(char* pcm, long pcm_len) {
+   const float MAX_8 = 127.0;
+   const float MIN_8 = -128.0;
+
+   float* ret = (float*)malloc(sizeof(float) * pcm_len);
+
+   for (long i = 0; i < pcm_len; i++) {
+      ret[i] = pcm[i] > 0 ? pcm[i]/MAX_8 : pcm[i]/MIN_8;
+   }
+
+   return ret;
+}
+float* twr_convert_16_bit_pcm(short* pcm, long pcm_len) {
+   const float MAX_16 = 32767.0;
+   const float MIN_16 = -32768.0;
+   
+   float* ret = (float*)malloc(sizeof(float) * pcm_len);
+
+   for (long i = 0; i < pcm_len; i++) {
+      ret[i] = pcm[i] > 0 ? pcm[i]/MAX_16 : pcm[i]/MIN_16;
+   }
+
+   return ret;
+}
+float* twr_convert_32_bit_pcm(long* pcm, long pcm_len) {
+   const float MAX_32 = 2147483647.0;
+   const float MIN_32 = -2147483648.0;
+   
+   float* ret = (float*)malloc(sizeof(float) * pcm_len);
+
+   for (long i = 0; i < pcm_len; i++) {
+      ret[i] = pcm[i] > 0 ? pcm[i]/MAX_32 : pcm[i]/MIN_32;
+   }
+
+   return ret;
 }
