6a0d49f6ec4928644c1aff2241ce7e7da4d2e7a4
Anthony <ajwood1965@gmail.com>
2024-09-19 09:35:00 -0800

    remove pong/out

--- examples/pong/out/jseventslib.d.ts
+++
@@ -1,6 +0,0 @@
-import { IWasmModule, IWasmModuleAsync, twrLibrary, TLibImports } from "twr-wasm";
-export default class twrLibExample extends twrLibrary {
-    imports: TLibImports;
-    registerKeyUpEvent(mod: IWasmModule | IWasmModuleAsync, eventID: number): void;
-}
-//# sourceMappingURL=jseventslib.d.ts.map
