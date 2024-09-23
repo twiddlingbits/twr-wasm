68fe9b799c1220c28c840107d45ae98e0542f04a
Anthony <ajwood1965@gmail.com>
2024-09-23 15:44:52 -0800

    update timer test var names

--- examples/tests-timer/tests-timer.c
+++ examples/tests-timer/tests-timer.c
@@ -3,7 +3,7 @@
 
 int t1_count=0;
 int t2_count=0;
-int timer2_id;
+int t2_id;
 
 // timer event callback (called once)
 __attribute__((export_name("on_timer1")))
@@ -24,7 +24,7 @@ void on_timer2(int event_id) {
    printf("timer callback 2 entered (event id=%d, count=%d)\n", event_id, t2_count);
 
    if (t2_count==5) {
-      twr_timer_cancel(timer2_id);
+      twr_timer_cancel(t2_id);
       if (t1_count!=0) 
          printf("twr_timer_single_shot FAIL!!\n");
    }
@@ -34,10 +34,10 @@ int tests_timer(int is_async) {
 
    printf("starting timer tests.\n");
 
-   int timer2=twr_register_callback("on_timer2");
-   timer2_id=twr_timer_repeat(500, timer2);
+   int t2_eventid=twr_register_callback("on_timer2");
+   t2_id=twr_timer_repeat(500, t2_eventid);
    
-   int timer1=twr_register_callback("on_timer1");
+   int t1_eventid=twr_register_callback("on_timer1");
 
    if (is_async) {
       printf("going to sleep...\n");
@@ -48,11 +48,11 @@ int tests_timer(int is_async) {
          printf("FAIL!!  t2_count is %d\n", t2_count);
       }
 
-      twr_timer_single_shot(2000, timer1);
+      twr_timer_single_shot(2000, t1_eventid);
    }
 
    else {
-      twr_timer_single_shot(4000, timer1);
+      twr_timer_single_shot(4000, t1_eventid);
    }
 
    return 0;
