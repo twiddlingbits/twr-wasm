b33f0ac8b194ca1455be02cb87b8a4f8d9a80a39
Anthony <ajwood1965@gmail.com>
2024-09-29 09:15:32 -0500

    add build (code) artifacts to main

--- lib-js/twreventqueue.js
+++ lib-js/twreventqueue.js
@@ -100,7 +100,7 @@ export class twrEventQueueReceive {
     }
     doCallbacks(upToIndex) {
         const end = upToIndex ? upToIndex : this.pendingEventIDs.length;
-        console.log("end", end, upToIndex, this.pendingEventIDs.length);
+        //console.log("end",end, upToIndex, this.pendingEventIDs.length);
         for (let i = 0; i < end; i++) {
             const eventID = this.pendingEventIDs[i];
             const args = this.pendingEventArgs[i];
